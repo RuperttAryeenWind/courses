6dd99a3be138462bce467a56ddf9d27edf20750a
Rupertt Aryeen Wind <ruperttaryeenwind@gmail.com>
2021-01-10 22:42:05 +0530

    Coursera - Nand 2 Tetris - Week 4 - Machine Language

--- Coursera /Build a Modern Computer from First Principles/Programming/04/fill/Fill.asm
+++ Coursera /Build a Modern Computer from First Principles/Programming/04/fill/Fill.asm
@@ -12,3 +12,71 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(LOOP)
+  // Fetch keyboard field
+  @KBD 
+  D=M
+
+  @IS_PRESSED
+  D;JGT
+  @NOT_PRESSED
+  D;JEQ
+
+// Function to execute when pressed
+(IS_PRESSED)
+  @R0
+  M=-1
+
+  // Continue to fill routine
+  @FILL
+  0;JMP
+
+// Function to execute when not pressed
+(NOT_PRESSED)
+  @R0
+  M=0
+
+  // Continue to fill routine
+  @FILL
+  0;JMP
+  
+(FILL)
+  @R0
+  D=M
+
+  // Fetch and store screen location
+  @SCREEN
+  D=A
+
+  @LOCATION
+  M=D
+
+(FLIP)
+  // Find target value to set pixel to
+  @R0
+  D=M
+
+  // Flip the pixel to target
+  @LOCATION
+  A=M
+  M=D
+
+  //Increment pixel
+  @LOCATION
+  D=M+1
+  @KBD
+  D=A-D
+
+  // Return to outer loop
+  @LOOP
+  D;JEQ
+
+  //Increment pixel
+  @LOCATION
+  M=M+1
+  A=M
+
+  // Continue flipping pixels
+  @FLIP
+  0;JMP
--- Coursera /Build a Modern Computer from First Principles/Programming/04/mult/mult.asm
+++ Coursera /Build a Modern Computer from First Principles/Programming/04/mult/mult.asm
@@ -7,3 +7,56 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+  @R2
+  M=0
+
+  // Check if R0 is zero
+  @R0
+  D=M
+  @END
+  D;JEQ
+
+  // Check if R1 is zero
+  @R1
+  D=M
+  @END
+  D;JEQ
+
+  // Setting counter as R0 + 1
+  @R0
+  D=M
+  @COUNTER 
+  M=D+1
+
+(SUM)
+  // Exit Condition
+  @COUNTER
+  D=M-1
+  
+  // Update counter with new value
+  M=D
+
+  @END
+  D;JEQ
+
+  // Summation
+  // Move partial mutliple to data registry
+  @R2
+  D=M
+
+  // Add value of R0 to data registry
+  @R1
+  D=D+M
+  
+  // Move new value of multi to memory
+  @R2
+  M=D
+
+  // Continue Looping
+  @SUM
+  0;JMP
+
+(END)
+  @END
+  0;JMP
+  
